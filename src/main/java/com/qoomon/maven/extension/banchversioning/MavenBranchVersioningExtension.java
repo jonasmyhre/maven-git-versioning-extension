6b095645ddb1e3573dd7bfc4a601f354daa77551
Bengt Brodersen <bengt.brodersen@deposit-solutions.com>
2016-11-09 15:00:58 +0100

    refactor: remove GVA class

--- src/main/java/com/qoomon/maven/extension/banchversioning/MavenBranchVersioningExtension.java
+++ src/main/java/com/qoomon/maven/extension/banchversioning/MavenBranchVersioningExtension.java
@@ -4,10 +4,10 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.apache.maven.AbstractMavenLifecycleParticipant;
 import org.apache.maven.MavenExecutionException;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Model;
-import org.apache.maven.model.Parent;
 import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
@@ -49,8 +49,7 @@ public class MavenBranchVersioningExtension extends AbstractMavenLifecyclePartic
         // check for semantic versioning format
         for (MavenProject project : projects) {
             if (!SEMVER_PATTERN.matcher(project.getVersion()).matches()) {
-                GAV gav = GAV.of(project);
-                throw new MavenExecutionException("Version Format validation", new IllegalArgumentException(gav + " version does not match semantic versioning pattern " + SEMVER_PATTERN));
+                throw new MavenExecutionException("Version Format validation", new IllegalArgumentException(project.getArtifact() + " version does not match semantic versioning pattern " + SEMVER_PATTERN));
             }
         }
 
@@ -61,13 +60,9 @@ public class MavenBranchVersioningExtension extends AbstractMavenLifecyclePartic
             FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder()
                     .findGitDir(gitDirectory);  // scan up the file system tree
 
-            String branchName;
             try (Repository repository = repositoryBuilder.build()) {
-                branchName = repository.getBranch();
+                updateProjectVersionsToBranchVersions(projects, repository);
             }
-
-            logger.info("current branch: " + branchName);
-            updateProjectVersionsToBranchVersions(projects, branchName);
 
         } catch (Exception e) {
             logger.error("", e);
@@ -75,52 +70,75 @@ public class MavenBranchVersioningExtension extends AbstractMavenLifecyclePartic
         }
     }
 
-    private void updateProjectVersionsToBranchVersions(List<MavenProject> projects, String branchName) throws IOException {
-        Map<GAV, String> versionMap = determineProjectVersions(projects, branchName);
+    private void updateProjectVersionsToBranchVersions(List<MavenProject> projects, Repository repository) throws IOException {
 
-        versionMap.forEach((gav, branchVersion) -> logger.info("set version to " + branchVersion + " @ " + gav));
+        Map<Artifact, String> versionMap = determineProjectVersions(projects, repository);
+
         for (MavenProject project : projects) {
             updateProjectVersion(versionMap, project);
         }
     }
 
-    private Map<GAV, String> determineProjectVersions(List<MavenProject> projects, String branchName) throws IOException {
+    private Map<Artifact, String> determineProjectVersions(List<MavenProject> projects, Repository repository) throws IOException {
 
-        Map<GAV, String> versionMap = Maps.newHashMap();
+        String branchName = repository.getBranch();
+        logger.debug("branch: " + branchName);
+        String commitHash = repository.resolve("HEAD").getName();
+        logger.debug("commit: " + commitHash);
+
+        boolean detachedHead = branchName.equals(commitHash);
+        if (!detachedHead) {
+            logger.info("Branch: " + branchName);
+        } else {
+            logger.info("Branch: (HEAD detached at " + commitHash + ")");
+        }
+
+        Map<Artifact, String> versionMap = Maps.newHashMap();
         for (MavenProject project : projects) {
+            String projectVersion = project.getVersion();
+            String releaseVersion = projectVersion.replaceFirst("-SNAPSHOT$", "");
+
             String branchVersion;
-            // Main Branch
-            if (mainReleaseBranch.equalsIgnoreCase(branchName)) {
-                branchVersion = project.getVersion().replaceFirst("-SNAPSHOT$","");
-            } else {
-                String branchNameUnified = branchName.replaceAll("[^A-Za-z0-9]", "_");
-                // Release Branches
-                if (releaseBranchPrefixSet.stream().anyMatch(prefix -> branchName.matches("^" + prefix))) {
-                    branchVersion = branchNameUnified + "-" + project.getVersion().replaceFirst("-SNAPSHOT$","");
-                }
-                // SNAPSHOT Branches
-                else {
-                    branchVersion = branchNameUnified + "-SNAPSHOT";
-                }
+            // Detached HEAD
+            if (detachedHead) {
+                branchVersion = commitHash;
             }
-            versionMap.put(GAV.of(project), branchVersion);
+            // Main Branch
+            else if (mainReleaseBranch.equalsIgnoreCase(branchName)) {
+                branchVersion = releaseVersion;
+            }
+            // Release Branches
+            else if (releaseBranchPrefixSet.stream().anyMatch(branchName::startsWith)) {
+                branchVersion = branchName + "-" + releaseVersion;
+            }
+            // SNAPSHOT Branches
+            else {
+                branchVersion = branchName + "-SNAPSHOT";
+            }
+
+            logger.info(project.getArtifact() + System.lineSeparator()
+                    + "       Branch version: " + branchVersion);
+
+            versionMap.put(project.getArtifact(), branchVersion);
         }
 
         return versionMap;
     }
 
-    private void updateProjectVersion(Map<GAV, String> newProjectVersionMap, MavenProject project) throws IOException {
-        GAV projectGav = GAV.of(project);
-
+    private void updateProjectVersion(Map<Artifact, String> newProjectVersionMap, MavenProject project) throws IOException {
         // --- update project ---
         {
             // update project parent version
             // No need to update parent version, because model is in memory
 
             // update project version
-            String projectVersionUpdated = newProjectVersionMap.get(projectGav);
+            String projectVersionUpdated = newProjectVersionMap.get(project.getArtifact());
+            logger.debug(project.getArtifactId() + " set version " + projectVersionUpdated);
             project.setVersion(projectVersionUpdated);
-            project.getArtifact().setVersionRange(VersionRange.createFromVersion(projectVersionUpdated));
+
+            VersionRange versionRange = VersionRange.createFromVersion(projectVersionUpdated);
+            logger.debug(project.getArtifactId() + " set version range " + projectVersionUpdated);
+            project.getArtifact().setVersionRange(versionRange);
         }
 
         // --- update project model ---
@@ -128,25 +146,27 @@ public class MavenBranchVersioningExtension extends AbstractMavenLifecyclePartic
             {
                 Model projectModel = project.getModel();
                 // update project model parent version
-                Parent projectModelParent = projectModel.getParent();
-                if (projectModelParent != null) {
+                if (projectModel.getParent() != null) {
                     // update parent version
-                    projectModelParent.setVersion(project.getParent().getVersion());
+                    logger.debug(project.getArtifactId() + " set model parent version " + project.getParent().getVersion());
+                    projectModel.getParent().setVersion(project.getParent().getVersion());
                 }
 
                 // update project model version
+                logger.debug(project.getArtifactId() + " set model version " + project.getVersion());
                 projectModel.setVersion(project.getVersion());
             }
             {
                 Model projectModelOriginal = project.getOriginalModel();
                 // update project model parent version
-                Parent projectModelOriginalParent = projectModelOriginal.getParent();
-                if (projectModelOriginalParent != null) {
+                if (projectModelOriginal.getParent() != null) {
                     // update parent version
-                    projectModelOriginalParent.setVersion(project.getParent().getVersion());
+                    logger.debug(project.getArtifactId() + " set original model parent version " + project.getParent().getVersion());
+                    projectModelOriginal.getParent().setVersion(project.getParent().getVersion());
                 }
 
                 // update project model version
+                logger.debug(project.getArtifactId() + " set original model version " + project.getVersion());
                 projectModelOriginal.setVersion(project.getVersion());
             }
 
@@ -156,12 +176,13 @@ public class MavenBranchVersioningExtension extends AbstractMavenLifecyclePartic
 
     private void updateProjectFile(MavenProject project) throws IOException {
         Model originalModel = project.getOriginalModel();
-        logger.debug("override temp pom");
         File newPomFile = File.createTempFile("pom", ".xml");
         newPomFile.deleteOnExit();
         try (FileWriter fileWriter = new FileWriter(newPomFile)) {
             new MavenXpp3Writer().write(fileWriter, originalModel);
-            project.setPomFile(newPomFile);
         }
+        logger.debug(project.getArtifactId() + " set pom file");
+        project.setPomFile(newPomFile);
     }
+
 }
